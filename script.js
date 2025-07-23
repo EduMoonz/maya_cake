66905ae644be95d7efa96bb7306806a6a0a97b12
EduMoonz <cedusantos7@outlook.com>
2025-07-23 20:25:31 -0300

    commit 7.1

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const yearText = document.getElementById("year");
   yearText.textContent = new Date().getFullYear();
 });
+
+// Efeito de digitação
+document.addEventListener("DOMContentLoaded", () => {
+  const text = "A vida parece muito melhor para quem ri com a boca lambuzada de chocolate!";
+  const typingTarget = document.getElementById("typing-text");
+  let i = 0;
+
+  function type() {
+    if (i < text.length) {
+      typingTarget.textContent += text.charAt(i);
+      i++;
+      setTimeout(type, 60);
+    }
+  }
+
+  type();
+});
